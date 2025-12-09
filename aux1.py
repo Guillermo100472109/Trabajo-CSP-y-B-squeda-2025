0d5129cf07c27833de94a92804ec90f09cd804ba
guille100472109 <100472109@alumnos.uc3m.es>
2025-12-09 21:28:42 +0100

    first commit

---
+++ aux1.py
@@ -0,0 +1,63 @@
+def print_decorated_matrix(board, size):
+    border = "+---" * size + "+"
+    print(border)
+    for i in range(size):
+        row_str = "|"
+        for j in range(size):
+            cell = board[(i * size) + j]
+            display_char = " " if cell == "." else cell
+            row_str += f" {display_char} |"
+        print(row_str)
+        print(border)
+        
+def print_solution_matrix(solution, size):
+    if not solution:
+        print("No se encontró ninguna solución.")
+        return
+
+    board = [solution[i] for i in range(size * size)]
+
+    border = "+---" * size + "+"
+    print(border)
+    for i in range(size):
+        row_str = "|"
+        for j in range(size):
+            cell = board[i * size + j]
+            row_str += f" {cell} |"
+        print(row_str)
+        print(border)
+
+def make_rows(n):
+    filas = []
+    for i in range(n):
+        inicio = i * n
+        fin = inicio + n
+        filas.append(list(range(inicio, fin)))
+    return filas
+
+def make_cols(n):
+    columnas = []
+    for i in range(n):
+        inicio = i
+        fin = n * n
+        salto = n
+        
+        columnas.append(list(range(inicio, fin, salto)))
+    return columnas
+
+
+def equal_colors(*args): # Restricciones horizontales y verticales
+        b = 0
+        n = 0
+        for i in args:
+            if i == "B":
+                b += 1
+            elif i == "N":
+                n += 1
+        return b == n
+
+def two_equal_colors(*args): # No más de dos colores seguidos
+    for i in range(len(args)-2):
+        if args[i] == args[i+1] == args[i+2] :
+            return False
+    return True
---
+++ parte-1.py
@@ -0,0 +1,74 @@
+import sys
+from constraint import *
+from aux1 import *
+
+
+def main():
+    if len(sys.argv) != 3:
+        print("Uso: python parte-1.py <fichero_entrada> <fichero_salida>")
+        sys.exit(1)
+    entrada = sys.argv[1]
+    salida = sys.argv[2]
+
+    tablero: list = []
+    entrada = open(entrada, "r")
+
+    # Lectura del fichero de entrada
+    with entrada as e:
+        n = int(e.readline().strip())
+        for _ in range(n):
+            linea = e.readline().strip()
+            tablero.extend(linea)
+
+    problem = Problem()
+
+    # Creacion de las variables con python-constraint
+    for position, cell_value in enumerate(tablero):
+        if cell_value == ".":
+            problem.addVariable(position, ["N", "B"])
+        elif cell_value == "N":
+            problem.addVariable(position, ["N"])
+        elif cell_value == "B":
+            problem.addVariable(position, ["B"])
+
+    # Añadir las restricciones al problema
+
+    rows = make_rows(n)
+    cols = make_cols(n)
+    #print(cols)
+    #print(rows)    
+
+
+    for i in rows:
+        problem.addConstraint(equal_colors, i)
+        problem.addConstraint(two_equal_colors,i)
+    for i in cols:
+        problem.addConstraint(equal_colors, i)
+        problem.addConstraint(two_equal_colors,i)
+
+
+    print("Estado inicial del tablero:")
+    print_decorated_matrix(tablero, n)
+
+    soluciones = problem.getSolutions()
+     
+    print(f"{len(soluciones)} soluciones encontradas")
+
+    original_stdout = sys.stdout
+    with open(salida, 'w') as f:
+        sys.stdout = f  # Redirigimos print al fichero
+        
+        print_decorated_matrix(tablero, n)
+        print("") # Separador
+        
+        if len(soluciones) > 0:
+            # Imprimimos la primera solución encontrada
+            # Convertimos el diccionario de solución a lista/matriz para tu función de print
+            solucion_dict = soluciones[0]
+            solucion_lista = [solucion_dict[i] for i in range(n*n)]
+            print_solution_matrix(solucion_lista, n)
+            
+        sys.stdout = original_stdout # Restauramos salida
+
+if __name__ == "__main__":
+    main()
